62c40ed217a99be60b5bdf61ea68e4c6076232e6
Eirik Sandberg <eirik.sandberg@live.no>
2020-02-24 14:56:33 +0100

    norwegian integration

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 import logging
 from globals import Globals
-from map_request import search_for_product, preprocess_query, get_best_quess_item
+from map_request import preprocess_query, get_best_quess_item
 from graphql import get_products
 
 # App setup
@@ -12,7 +12,8 @@ app = Flask(__name__)
 def search_product_catalog():
     data = request.get_json()
     user_query = data['query']
-    products_df = search_for_product(user_query)
+    preprocessed_query = preprocess_query(user_query, format='str')
+    products_df = get_products(preprocessed_query)
     products_json = products_df.to_json(orient='records')
     return products_json
 
@@ -67,7 +68,8 @@ def get_best_guess_item_from_kindly():
     pattern_key = data['exchange']['pattern_key']
     pattern = data['context'][pattern_key]
     user_query = data['message'].replace(pattern, "")
-    item_json = get_best_quess_item(user_query)[0]
+    preprocessed_query = preprocess_query(user_query, format='str')
+    item_json = get_best_quess_item(preprocessed_query)[0]
     return {'reply': 'Hva med ' + item_json['name'] + '?', 'image': item_json['image']['url'],
             "buttons": [
             {
--- map_request.py
+++ map_request.py
@@ -1,12 +1,13 @@
 from graphql import get_products, get_best_quess_item
 import nltk
 import en_core_web_sm
+from translate.translate import Translator
 
 import spacy
 from sense2vec import Sense2VecComponent, Sense2Vec
 
 nlp = spacy.load("en_core_web_sm")
-#s2v = Sense2Vec().from_disk("s2v_reddit_2015_md/s2v_old")
+# s2v = Sense2Vec().from_disk("s2v_reddit_2015_md/s2v_old")
 colors = ['lily', 'liliac', 'rain', 'peach', 'lava', 'forest']
 
 nltk.download('punkt')
@@ -20,7 +21,7 @@ ACCEPTED_SEARCH_WORD_CLASSES = ['NOUN', 'ADJ', 'PROPN']
 def similarity(query):
     similarities = []
     for token in query:
-        for color in color_representation():
+        for color in find_all_senses(colors):
             try:
                 similarities.append((token, color, s2v.similarity(token, color)))
             except:
@@ -29,11 +30,10 @@ def similarity(query):
     return similarities
 
 
-def search_for_product(preprocessed_query):
-    return get_products(preprocessed_query)
-
-
 def preprocess_query(query, format=None):
+    translator = Translator('en', 'no')
+    query = translator.translate(query)
+    print(query)
     doc = nlp(query)
     tagged = [(x.orth_, x.pos_, x.lemma_) for x in doc if not x.is_stop and x.pos_ != 'PUNCT']
 
@@ -47,27 +47,33 @@ def preprocess_query(query, format=None):
                 search_query.append(lemma)
         return " ".join(search_query)
 
-    elif format == 'sense2vec':
-        search_query = []
-        for token, word_class, lemma in tagged:
-            if word_class in ACCEPTED_SEARCH_WORD_CLASSES:
-                search_query.append('{}|{}'.format(token, word_class.upper()))
-        return search_query
+    else:
+        raise ValueError('Format ' + format + ' is not supported')
 
 
-def color_representation():
-    cs = []
-    for c in colors:
-        processed = nlp(c)[0]
-        formated = '{}|{}'.format(c, processed.pos_)
+def preprocess_to_sense2vec(query):
+    doc = nlp(query)
+    tagged = [(x.orth_, x.pos_, x.lemma_) for x in doc if not x.is_stop and x.pos_ != 'PUNCT']
+    search_query = []
+    for token, word_class, lemma in tagged:
+        if word_class in ACCEPTED_SEARCH_WORD_CLASSES:
+            search_query.append('{}|{}'.format(token, word_class.upper()))
+    return search_query
+
+
+def find_all_senses(tokens):
+    senses = []
+    for token in tokens:
+        processed = nlp(token)[0]
+        formated = '{}|{}'.format(token, processed.pos_)
         other_senses = s2v.get_other_senses(formated)
-        cs = cs + [formated] + other_senses
-    return cs
+        senses = senses + [formated] + other_senses
+    return senses
 
 
 if __name__ == '__main__':
     query = "i feel moody"
-    d = preprocess_query(query, format='sense2vec')
+    d = preprocess_to_sense2vec(query)
     similarity(d)
     exit()
 
